307699803c9cee2d3f16bf3b618bf6904873c105
Brayan Diaz C <brayandiazc@gmail.com>
2021-05-13 17:46:24 -0500

    Add state model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_07_221618) do
+ActiveRecord::Schema.define(version: 2021_05_13_223112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "states", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: ""
