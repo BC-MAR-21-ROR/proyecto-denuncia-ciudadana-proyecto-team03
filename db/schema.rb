73994f8812be6994dea986b929ceb64012dfcdc0
Brayan Diaz C <brayandiazc@gmail.com>
2021-05-13 18:05:56 -0500

    Add zip model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_13_230337) do
+ActiveRecord::Schema.define(version: 2021_05_13_230539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,15 @@ ActiveRecord::Schema.define(version: 2021_05_13_230337) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "zips", force: :cascade do |t|
+    t.bigint "settlement_id", null: false
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["settlement_id"], name: "index_zips_on_settlement_id"
+  end
+
   add_foreign_key "municipalities", "states"
   add_foreign_key "settlements", "municipalities"
+  add_foreign_key "zips", "settlements"
 end
