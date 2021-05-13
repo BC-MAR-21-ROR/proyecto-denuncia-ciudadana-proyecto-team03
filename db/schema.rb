8f6ed4e3e73d565bf2bd44ba913c29c547381dc3
Brayan Diaz C <brayandiazc@gmail.com>
2021-05-13 17:55:13 -0500

    Add municipality model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_13_223112) do
+ActiveRecord::Schema.define(version: 2021_05_13_225439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "municipalities", force: :cascade do |t|
+    t.bigint "state_id", null: false
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["state_id"], name: "index_municipalities_on_state_id"
+  end
 
   create_table "states", force: :cascade do |t|
     t.string "name"
@@ -39,4 +47,5 @@ ActiveRecord::Schema.define(version: 2021_05_13_223112) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "municipalities", "states"
 end
