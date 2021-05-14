f0b35f9474e34fa24a2064d8e93febf6f06b858d
Brayan Diaz C <brayandiazc@gmail.com>
2021-05-14 15:09:40 -0500

    Feat complaint scaffold create

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_13_230539) do
+ActiveRecord::Schema.define(version: 2021_05_14_200835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "complaints", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.bigint "category_id", null: false
+    t.date "date_of_events"
+    t.bigint "state_id", null: false
+    t.bigint "municipality_id", null: false
+    t.bigint "zip_id", null: false
+    t.bigint "settlement_id", null: false
+    t.string "street"
+    t.string "number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_complaints_on_category_id"
+    t.index ["municipality_id"], name: "index_complaints_on_municipality_id"
+    t.index ["settlement_id"], name: "index_complaints_on_settlement_id"
+    t.index ["state_id"], name: "index_complaints_on_state_id"
+    t.index ["zip_id"], name: "index_complaints_on_zip_id"
+  end
 
   create_table "municipalities", force: :cascade do |t|
     t.bigint "state_id", null: false
@@ -24,11 +50,11 @@ ActiveRecord::Schema.define(version: 2021_05_13_230539) do
   end
 
   create_table "settlements", force: :cascade do |t|
-    t.bigint "municipality_id", null: false
+    t.bigint "zip_id", null: false
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["municipality_id"], name: "index_settlements_on_municipality_id"
+    t.index ["zip_id"], name: "index_settlements_on_zip_id"
   end
 
   create_table "states", force: :cascade do |t|
@@ -56,14 +82,19 @@ ActiveRecord::Schema.define(version: 2021_05_13_230539) do
   end
 
   create_table "zips", force: :cascade do |t|
-    t.bigint "settlement_id", null: false
+    t.bigint "municipality_id", null: false
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["settlement_id"], name: "index_zips_on_settlement_id"
+    t.index ["municipality_id"], name: "index_zips_on_municipality_id"
   end
 
+  add_foreign_key "complaints", "categories"
+  add_foreign_key "complaints", "municipalities"
+  add_foreign_key "complaints", "settlements"
+  add_foreign_key "complaints", "states"
+  add_foreign_key "complaints", "zips"
   add_foreign_key "municipalities", "states"
-  add_foreign_key "settlements", "municipalities"
-  add_foreign_key "zips", "settlements"
+  add_foreign_key "settlements", "zips"
+  add_foreign_key "zips", "municipalities"
 end
