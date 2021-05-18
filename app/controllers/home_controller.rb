9b05679115ea86cf966e1c7b828ca0f47f5b9973
carlos <crmm17.cm@gmail.com>
2021-05-18 12:31:51 -0500

    Add complaints in my places of interest in home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    my_interested_settlements = current_user.interest_places.pluck(:settlement_id)
+    @complaints = Complaint.where(settlement_id: my_interested_settlements).order('created_at DESC')
   end
 end
