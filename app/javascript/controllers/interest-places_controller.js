33377ea9a57ef0061fa92b87d5fc8642ac9ae247
carlos <crmm17.cm@gmail.com>
2021-05-25 13:02:46 -0500

    Fix interest-places_controller javascript and remove unecessary files

--- app/javascript/controllers/interest-places_controller.js
+++ app/javascript/controllers/interest-places_controller.js
@@ -6,26 +6,30 @@ export default class extends Controller {
     this.changeSettlements();
   }
 
-  changeSettlements() {
-    let postal_id = this.postal_idTarget.value;
-    let current_value = this.settlement_idTarget.dataset.value;
+  async changeSettlements() {
+    const postal_id = this.postal_idTarget.value;
+    const current_value = this.settlement_idTarget.dataset.value;
     // console.log(postal_id)
     if (postal_id) {
-      fetch('/postals/'+postal_id+'.json').then(resp => resp.json())
-        .then(resp => {
-          // console.log(resp);
-          this.settlement_idTarget.innerHTML = '';
-          this.settlement_idTarget.appendChild(document.createElement("option"));
-          resp.settlements.forEach(sett => {
-            const option = document.createElement("option");
-            option.value = sett.id;
-            option.innerHTML = sett.name;
-            if (sett.id == current_value) {option.setAttribute('selected', 'selected')}
-            this.settlement_idTarget.appendChild(option);
-          });
-        }).catch(err => {
-          console.log(err);
-        });
+      const response = await fetch(`/postals/${postal_id}.json`);
+
+      if (!response.ok) {
+        const message = `The following error has occured: ${response.status}`;
+        throw new Error(message);
+      }
+
+      const settlements = await response.json();
+
+      this.settlement_idTarget.innerHTML = '';
+      this.settlement_idTarget.appendChild(document.createElement("option"));
+      settlements.settlements.forEach(sett => {
+        const option = document.createElement("option");
+        option.value = sett.id;
+        option.innerHTML = sett.name;
+        if (sett.id == current_value) {option.setAttribute('selected', 'selected')}
+        this.settlement_idTarget.appendChild(option);
+      });
+
     }
   }
 }
--- app/views/zips/_zip.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! zip, :id, :municipality_id, :name, :created_at, :updated_at
-json.settlements zip.settlements
