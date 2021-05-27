ec8e5ade3a52e3e9bb0b5287d4d77e3ceb80d1a9
Brayan Diaz C <brayandiazc@gmail.com>
2021-05-26 20:10:42 -0500

    Style to new session form

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
   resources :interest_places
   resources :complaints
   devise_for :users
+
   devise_scope :user do
-  	authenticated :user do
-  	  root to: "home#index"
-  	end
-  	unauthenticated do
-  	  root 'devise/sessions#new', as: :unauthenticated_root
-  	end
+    authenticated :user do
+      root to: "home#index"
+    end
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
   end
 end
