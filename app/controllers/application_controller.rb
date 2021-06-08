460fd4ddb8087bdd403f68a6bb80c281a649cc9b
Brayan Diaz C <brayandiazc@gmail.com>
2021-06-08 16:48:06 -0500

    FEAT: Redirect view after registration
    Github issue: #42

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,4 +20,8 @@ class ApplicationController < ActionController::Base
     def after_sign_out_path_for(_resource_or_scope)
       new_user_session_path
     end
+
+    def after_inactive_sign_up_path_for(_resource)
+      new_user_session_path
+    end
 end
